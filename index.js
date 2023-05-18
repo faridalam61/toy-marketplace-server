551c856b671374633bbe8ccc64c52333c2566fe3
faridalam61 <faridalam62@gmail.com>
2023-05-18 18:56:21 +0600

    Display all toys added

--- index.js
+++ index.js
@@ -42,7 +42,12 @@ async function run() {
       res.send(result)
   })
 
-
+  // Get All Toys
+  app.get('/toys', async(req, res) => {
+    const cursor = toyCollection.find();
+    const result = await cursor.toArray()
+    res.send(result)
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
