1a32bfcea9e868229a4fec09b64c8f1d3e4d9658
faridalam61 <faridalam62@gmail.com>
2023-05-20 10:32:59 +0600

    Vercel configuration updated

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    maxPoolSize:10,   
 });
 
 async function run() {
